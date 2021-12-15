290129e7b154fd9f4b439c2b53996d54f45f38ab
Antonas <antonas@coral.lt>
2021-12-15 18:01:29 +0300

    12

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,10 +4,21 @@ import './App.css';
 const App = () => {
     return ( 
         <div className='app-wrapper'>
-            <header>
-                <img src='https://assets.hongkiat.com/uploads/logo-parodies/starbucks-coffee_star-wars-coffee.jpg?newedit'/>
+            <header className='header'>
+                <img src='https://logodix.com/logo/1917404.png'/>
             </header>
-            <nav></nav>
+            <nav className='nav'>
+                <ul>
+                    <li><a href='#'>Profile</a></li>
+                    <li><a href='#'>Messages</a></li>
+                    <li><a href='#'>News</a></li>
+                </ul>
+            </nav>
+            <div className='content'>Main content
+                <div><img src="https://wallpapercave.com/wp/wp7522889.jpg"/></div>
+                <div>ava + description</div>
+                <div>posts</div>
+            </div>
 
         </div>
     )
